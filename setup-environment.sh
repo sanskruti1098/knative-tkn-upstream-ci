e196d503905315fef18eb4f42f5801099dce69bc
Siddhesh Ghadi <Siddhesh.Ghadi@ibm.com>
2022-04-05 21:29:25 +0530

    Update script to use submodule & ci-script stored in gcp

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# **** This script is to be ran on upstream knative ci server. ****
+# **** This script runs on upstream knative ci server from ci-script which is stored in GCP. ****
 # It sets up the k8s environment and updates the knative source for succesfully test run.
 
 set -e
 
 BASE_DIR=/opt/knative-upstream-ci
-K8S_AUTOMN_DIR=${BASE_DIR}/k8s-automation
+K8S_AUTOMN_DIR=${BASE_DIR}/k8s-ansible-automation
 SSH_USER=root
 SSH_HOST="cluster.ppc64le"
 SSH_ARGS="-i /opt/cluster/knative.pem -o MACs=hmac-sha2-256 -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
 
-## Setup host entries so that access to power machines is possible using custom domains
-if [ -z ${1} ]
+# exit if CI_JOB is not set
+if [ -z ${CI_JOB} ]
 then
-    echo "Please provide ssh host IP as first argument."
+    echo "Missing CI_JOB variable"
     exit 1
 fi
-echo "${1} cluster.ppc64le registry.ppc64le ppc64le" >> /etc/hosts
 
 ## Trigger k8s automation on remote power machines
 echo "Creating k8s cluster...."
@@ -27,6 +26,8 @@ then
     echo "Cluster creation failed."
     exit 1
 fi
+
+echo "Setting up access to k8s cluster...."
 # copy access files
 scp ${SSH_ARGS} ${SSH_USER}@${SSH_HOST}:${K8S_AUTOMN_DIR}/share/* /tmp
 # setup docker access
@@ -40,13 +41,14 @@ mv /tmp/config $HOME/.kube/
 echo 'Cluster created successfully'
 
 ## Fetch & run adjust.sh script to patch the source code with image replacements and other fixes
-## Introducing JOB var which can be used to fetch adjust script based on repo-tag
-## $JOB needs to be set in knative upstream job configurations
-touch /tmp/adjust.sh
-if [ ${JOB} == "eventing-main" ]
+## Introducing CI_JOB var which can be used to fetch adjust script based on repo-tag
+## $CI_JOB needs to be set in knative upstream job configurations
+echo 'Patching source code with ppc64le specific changes....'
+echo 'echo "No code ppc64le specific changes required."' > /tmp/adjust.sh
+if [ ${CI_JOB} == "eventing-main" ]
 then
     scp ${SSH_ARGS} ${SSH_USER}@${SSH_HOST}:${BASE_DIR}/adjust/eventing/main/* /tmp/
-elif [ ${JOB} == "eventing-release-0.23" ]
+elif [ ${CI_JOB} == "eventing-release-0.23" ]
 then
     scp ${SSH_ARGS} ${SSH_USER}@${SSH_HOST}:${BASE_DIR}/adjust/eventing/0.23/* /tmp/
 fi
