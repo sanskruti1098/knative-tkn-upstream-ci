c1b7061303aeb6567da0ac90e1dbfed7c02e5244
Md.afsan Hossain <Mdafsan.Hossain@ibm.com>
2022-05-31 13:16:53 +0530

    Remove destroy.sh from adjust script

--- adjust/client/main/adjust.sh
+++ adjust/client/main/adjust.sh
@@ -4,5 +4,4 @@ cmd_line="./kn service create svc1 --no-wait --image \$img -e TARGET=Knative -n 
 
 go build -o kn cmd/kn/main.go
 sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
-sed -i '/success.*/i\  .\/destroy.sh' test/e2e-tests.sh
 sed -i "/sleep.*/a\  ${cmd_line}" test/e2e-tests.sh
