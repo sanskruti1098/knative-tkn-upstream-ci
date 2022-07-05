85702ce73b1dd808fa961d2646c58a6a310d37e5
mdafsanhossain <Mdafsan.Hossain@ibm.com>
2022-07-05 16:38:01 +0530

    Correct sed command for e2e timeout
    Signed-off-by: mdafsanhossain <Mdafsan.Hossain@ibm.com>

--- adjust/operator/main/adjust.sh
+++ adjust/operator/main/adjust.sh
@@ -3,4 +3,4 @@
 #Export USER before test starts
 sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
 #Increase e2e timeout to 60m
-sed -i "s/\(go_test_e2e.*\)timeout=45m\(.*\).*/\1timeout=60m\2/g" test/e2e-tests.sh
+sed -i "s/\(go_test_e2e.*\)timeout=20m\(.*\).*/\1timeout=60m\2/g" test/e2e-tests.sh
