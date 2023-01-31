3901458bfe4b02fccce4c2d3f6cd2c159c21b098
mdafsanhossain <Mdafsan.Hossain@ibm.com>
2023-01-31 13:51:02 +0530

    Adjustment for knative 1.9 release
    Signed-off-by: mdafsanhossain <Mdafsan.Hossain@ibm.com>

---
+++ adjust/serving/contour/release-1.9/adjust.sh
@@ -0,0 +1,15 @@
+# Export USER before test starts, otherwise a test stops
+sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
+sed -i "/^initialize.*/a export SHORT=1" test/e2e-tests.sh
+# Slow down an interval of kapp checking a status of k8s cluster otherewise will face 'connection refused' frequently
+sed -i 's/\(.*run_kapp deploy\)\(.*\)/\1 --wait-check-interval=30s --wait-concurrency=1 --wait-timeout=30m\2/' test/e2e-common.sh
+
+# Decrease a level of parallelism to 1 (the same as the number of worker nodes in KinD)
+sed -i "s/^\(parallelism=\).*/\1\"-parallel 1\"/" test/e2e-tests.sh
+
+# Downgrade the contour release in third_party/contour-latest to release 1.6.0 (due to Envoy)
+curl --connect-timeout 10 --retry 5 -L https://github.com/knative-sandbox/net-contour/releases/download/knative-v1.6.0/contour.yaml -o third_party/contour-latest/contour.yaml
+curl --connect-timeout 10 --retry 5 -L https://github.com/knative-sandbox/net-contour/releases/download/knative-v1.6.0/net-contour.yaml -o third_party/contour-latest/net-contour.yaml
+
+#Place overlay
+cp /tmp/overlay-ppc64le.yaml test/config/ytt/core/overlay-ppc64le.yaml
---
+++ adjust/serving/kourier/release-1.9/adjust.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Export USER before test starts, otherwise a test stops
+sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
+sed -i "/^initialize.*/a export SHORT=1" test/e2e-tests.sh
+#Fix Kapp naming
+sed -i 's|github\.com/k14s/kapp/cmd/kapp|github\.com/vmware-tanzu/carvel-kapp/cmd/kapp|g' test/e2e-common.sh
+
+# Slow down an interval of kapp checking a status of k8s cluster otherewise will face 'connection refused' frequently
+sed -i 's/\(.*run_kapp deploy\)\(.*\)/\1 --wait-check-interval=30s --wait-concurrency=1 --wait-timeout=30m\2/' test/e2e-common.sh
+# Decrease a level of parallelism to 1 (the same as the number of worker nodes in KinD)
+sed -i "s/^\(parallelism=\).*/\1\"-parallel 1\"/" test/e2e-tests.sh
+# Set the number of replicas to 1 for stable test results
+sed -i 's/\(.*replicas: \).*/\11/' test/config/ytt/ingress/kourier/kourier-replicas.yaml
+
+#Place overlay
+cp /tmp/overlay-ppc64le.yaml test/config/ytt/core/overlay-ppc64le.yaml
