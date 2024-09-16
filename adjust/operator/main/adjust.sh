528dcdb12c4e4ac019871bb4ae4d5511f91654ac
Valen Mascarenhas <Valen.Mascarenhas@ibm.com>
2024-09-16 10:12:26 +0530

    adjustment scripts modified for operator (#50)

--- adjust/operator/main/adjust.sh
+++ adjust/operator/main/adjust.sh
@@ -4,7 +4,4 @@
 sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
 #Increase e2e timeout to 60m
 sed -i "s/\(go_test_e2e.*\)timeout=20m\(.*\).*/\1timeout=40m\2/g" test/e2e-tests.sh
-sed -i "/^success.*/i .\/destroy.sh $1" test/e2e-tests.sh
-sed -i "/.*dump_cluster_state().*/a\  .\/destroy.sh $1" vendor/knative.dev/hack/infra-library.sh
-kubectl get cm vcm-script -n default -o jsonpath='{.data.script}' > destroy.sh && chmod +x destroy.sh
 echo "Source code patched successfully"
