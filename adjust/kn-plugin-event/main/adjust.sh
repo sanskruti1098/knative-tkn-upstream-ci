671748d1f39f257a946164f922560815e5a7eb32
valen-mascarenhas14 <100989126+valen-mascarenhas14@users.noreply.github.com>
2025-03-29 13:01:59 +0530

    golangci timeout increased to 15m (#27)

--- adjust/kn-plugin-event/main/adjust.sh
+++ adjust/kn-plugin-event/main/adjust.sh
@@ -1,4 +1,5 @@
 sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
 sed -i "/^initialize.*/a sleep 60" test/e2e-tests.sh
+sed -i 's/timeout: 5m/timeout: 15m/' .golangci.yaml
 sed -i "/^sleep.*/a kubectl set image deployment/3scale-kourier-gateway kourier-gateway=icr.io/upstream-k8s-registry/knative/maistra/envoy:v2.4 -n kourier-system" test/e2e-tests.sh
 sed -i 's|gcr.io/knative-samples/helloworld-go|quay.io/openshift-knative/client/helloworld:v1.9|g' pkg/k8s/test/addressresolver_cases.go
