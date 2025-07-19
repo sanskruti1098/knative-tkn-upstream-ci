af0ff5577fd459d40c1d6079f224ecbecfd6b925
Kumar-Abhishek <97608374+kabhiibm@users.noreply.github.com>
2025-07-19 23:53:29 +0530

    Update ppc64le.patch in release-1.18 for STRIMZI_KUBERNETES_VERSION
    Update ppc64le.patch
    
    Update ppc64le.patch
    
    Update ppc64le.patch
    
    Update ppc64le.patch
    
    Update adjust.sh
    
    release-1.18 patche updated

--- adjust/eventing-kafka-broker/release-1.18/adjust.sh
+++ adjust/eventing-kafka-broker/release-1.18/adjust.sh
@@ -4,5 +4,7 @@ sed -i "/^source.*/a export USER=$\(whoami\)" test/e2e-tests.sh
 #Increase e2e timeout to 60m
 sed -i "s/\(go_test_e2e.*\)timeout=20m\(.*\).*/\1timeout=40m\2/g" test/e2e-tests.sh
 # ppc64le.patch is already copied to tmp during setup-environment.sh run
+sed -i "s|K8S_VER_MAJOR|$(echo "$K8S_BUILD_VERSION" | sed -E 's/^v([0-9]+)\.([0-9]+)\..*/\1/')|" /tmp/ppc64le.patch
+sed -i "s|K8S_VER_MINOR|$(echo "$K8S_BUILD_VERSION" | sed -E 's/^v([0-9]+)\.([0-9]+)\..*/\2/')|" /tmp/ppc64le.patch
 git apply /tmp/ppc64le.patch
 echo "Source code patched successfully"
